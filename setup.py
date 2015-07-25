e935f1f03a1b2ad2bf7d5be8665abb176b68ad4a
Christopher Michael Dee <chris.dee@optimizely.com>
2015-07-25 01:14:24 -0700

    implemented base SSO for Docebo

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='docebo-SSO',
+    version='0.0.0',
+    description='Single Sign On Implementation for the Docebo LMS',
+    author='Chris Dee',
+    author_email='chris.dee@optimizely.com',
+    url='http://github.com/optimizely/docebo-SSO',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(
+      exclude=['tests']
+    ),
+    install_requires=[
+      'requests>=2.6.0',
+    ],
+    test_suite='sso_method_tests',
+)
